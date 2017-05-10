6a60fb333a3e19fb90eaa03fc31d6f18284b5ff3
martonMeszaros <marton.m95@gmail.com>
2017-05-10 19:02:12 +0200

    Created querry selection logic in main.py

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 """."""
 import psycopg2
 
+import menu_sys
+import querries
+
 
 def main():
+    """."""
     conn = psycopg2.connect("dbname=meszi user=meszi")
     cur = conn.cursor()
+
+    running = True
+    while running:
+        user_input = menu_sys.menu()
+        if user_input == "0":
+            running = False
+            break
+        elif user_input == "1":
+            cur.execute(querries.querries()[0][1])
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            pass
+        elif user_input == "4":
+            pass
+        elif user_input == "5":
+            pass
+        elif user_input == "6":
+            pass
+        elif user_input == "7":
+            pass
 
     cur.close()
     conn.close()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
